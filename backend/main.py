4feda4b19c51ef0f48a33825fabb8f45d710c438
Gustavo Henrique <gustavo@Lorenas-MacBook-Pro-2.local>
2020-08-02 00:09:08 -0300

    Add backend test functionality

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from http import HTTPStatus
+import json
+
+from flask import Flask, request
+from schematics.exceptions import DataError, ValidationError
+from schematics.models import Model
+from schematics.types import DateTimeType, IntType, StringType
+
+
+class LoggingInfo(Model):
+    id = IntType(required=True)
+    userId = StringType(required=True)
+    actionType = StringType(required=True)
+    timestamp = DateTimeType(required=True)
+    extra = StringType(required=False)
+
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/log', methods=['POST'])
 def receive_log():
-    return {'message': 'Log received!'}
+    try:
+        log_data = LoggingInfo(request.json)
+        log_data.validate()
+    except (ValidationError, DataError) as data_exception:
+        return {'error': json.loads(str(data_exception))}, HTTPStatus.BAD_REQUEST
+    print(log_data.to_primitive())
+    return '{"message": "Logged successfully"}'
 
 
